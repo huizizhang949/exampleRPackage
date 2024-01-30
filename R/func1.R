9005564ba5a8076c754c34266d5c0bc5ad70248a
Huizi Zhang <zhanghuizi98@outlook.com>
2024-01-30 22:06:43 +0800

    Add documentation for func1.R

--- R/func1.R
+++ R/func1.R
@@ -1,3 +1,15 @@
+#' Title
+#'
+#' @description
+#' A function to double the value of the input.
+#'
+#' @param x A vector.
+#'
+#' @return Twice the value of the input.
+#' @export
+#'
+#' @examples
+#' func(c(1,2))
 func1 <- function(x){
   return(2*x)
 }
